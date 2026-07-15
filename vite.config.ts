7b125d474580589b4a3f3b29254ea27214f8bf94
Invictus Navarchus <github.com.everglade984@passmail.com>
2026-07-15 12:01:02 +0700

    chore: add cf-headers for managing http headers

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,26 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 import { defineConfig } from 'vite';
-
+import { cfHeaders } from '@navarchus/cf-headers/vite';
+import { securityHeadersPreset } from '@navarchus/cf-headers';
 import { cloudflare } from '@cloudflare/vite-plugin';
 
 export default defineConfig(() => {
 	return {
-		plugins: [react(), tailwindcss(), cloudflare()],
+		plugins: [
+			react(),
+			tailwindcss(),
+			cloudflare(),
+			cfHeaders({
+				rules: [
+					securityHeadersPreset('/*', {
+						hsts: {
+							preload: true,
+						},
+					}),
+				],
+			}),
+		],
 		resolve: {
 			alias: {
 				'@': path.resolve(__dirname, './src'),
